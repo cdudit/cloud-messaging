84a41f8c72ec5d7a7b41698aa5f635f8f80ace9c
cdudit <clement.dudit85@gmail.com>
2020-12-03 11:58:47 +0100

    login formulaire ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { SubscribePage } from '../subscribe/subscribe.page';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  form: FormGroup;
 
-  constructor() {}
+  constructor(
+    public fb: FormBuilder,
+    public router: Router,
+    public modalController: ModalController
+  ) { }
 
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      id: ['', Validators.required],
+      mdp: ['', Validators.required]
+    });
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+  async goSubscribe() {
+    const modal = await this.modalController.create({
+      component: SubscribePage,
+      swipeToClose: true
+    });
+    return await modal.present();
+  }
 }
