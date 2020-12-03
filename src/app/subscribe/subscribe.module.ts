b25dab7522eec0c55945b7e9169592cab52897d1
cdudit <clement.dudit85@gmail.com>
2020-12-03 14:49:15 +0100

    subscribe ok

--- src/app/subscribe/subscribe.module.ts
+++ src/app/subscribe/subscribe.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { SubscribePage } from './subscribe.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    SubscribePageRoutingModule
+    SubscribePageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [SubscribePage]
 })
-export class SubscribePageModule {}
+export class SubscribePageModule { }
--- src/app/subscribe/subscribe.page.ts
+++ src/app/subscribe/subscribe.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-subscribe',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscribe.page.scss'],
 })
 export class SubscribePage implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    public fb: FormBuilder,
+    public router: Router,
+    public modalController: ModalController
+  ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      nom_utilisateur: ['', Validators.required],
+      email: ['', Validators.required],
+      mdp: ['', Validators.required],
+      confirm_mdp: ['', Validators.required],
+      naissance: ['', Validators.required],
+      adresse: ['', Validators.required],
+      ville: ['', Validators.required],
+      cp: ['', Validators.required]
+    });
   }
 
+  dismiss() {
+    this.modalController.dismiss();
+  }
 }
